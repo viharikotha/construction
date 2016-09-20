5bde9aa8c3c330bcd207b87f4f5408f656ff519f
viharikotha <vihari.wenso@gmail.com>
2016-09-20 14:05:10 +0530

    config file
    config file changed

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'wenso123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost'); 
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');

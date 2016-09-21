c97110c2e31b1148d0af7691466fc7eeb2f1e61b
viharikotha <vihari.wenso@gmail.com>
2016-09-21 17:12:31 +0530

    config
    config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'databse');
+define('DB_NAME', 'databa');
 
 /** MySQL database username */
 define('DB_USER', 'root');

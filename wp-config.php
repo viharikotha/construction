5c39f471b3d5738cf87bb235ae1a146e7bfcf953
viharikotha <vihari.wenso@gmail.com>
2016-09-21 17:10:30 +0530

    config file committed
    config file committed

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'con');
+define('DB_NAME', 'databse');
 
 /** MySQL database username */
 define('DB_USER', 'root');
